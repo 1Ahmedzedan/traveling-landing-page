df8c3810bff80be6beeedddfea134b7a60b3e8b9
Ahmed Zedan <zedan@gmail.com>
2023-09-12 14:37:52 +0300

    install gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Main from "./MainPage";
 import Services from "./ServicesPage";
 import Trips from "./TripsPage";
+import Footer from "./Footer";
+
 
 export default function App() {
   return (
@@ -13,31 +15,3 @@ export default function App() {
   );
 }
 
-function Footer(){
-  return(
-    <footer>
-      <div className="column">
-        <p>Restaurant Reservations</p>
-        <p>Flight Finder</p>
-        <p>Hotels</p>
-        <p>Destinations</p>
-        <p>Hotels</p>
-        <p>Cities</p>
-        <p>Airports</p>
-      </div>
-      <div className="column">
-        <p>Reviews</p>
-        <p>Discover</p>
-        <p>Seasonal holidays deals</p>
-      </div>
-      <div className="column">
-        <p>Copy rights: 2023 @travelling agency.</p>
-        <div className="social">
-          <img src="img/face.svg" alt="facebook"/>
-          <img src="img/insta.svg" alt="insta"/>
-          <img src="img/twitter.svg" alt="twitter"/>
-        </div>
-      </div>
-    </footer>
-  );
-}
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+export default function Footer(){
+    return(
+      <footer>
+        <div className="column">
+          <p>Restaurant Reservations</p>
+          <p>Flight Finder</p>
+          <p>Hotels</p>
+          <p>Destinations</p>
+          <p>Hotels</p>
+          <p>Cities</p>
+          <p>Airports</p>
+        </div>
+        <div className="column">
+          <p>Reviews</p>
+          <p>Discover</p>
+          <p>Seasonal holidays deals</p>
+        </div>
+        <div className="column">
+          <p>Copy rights: 2023 @travelling agency.</p>
+          <div className="social">
+            <img src="img/face.svg" alt="facebook"/>
+            <img src="img/insta.svg" alt="insta"/>
+            <img src="img/twitter.svg" alt="twitter"/>
+          </div>
+        </div>
+      </footer>
+    );
+  }
